73d9b4bc6fb28256d9170a7f04f6c871dc715b3d
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-12-09 10:11:51 +0900

    APPVEYOR_REPO_NAME と APPVEYOR_PULL_REQUEST_HEAD_REPO_NAME のチェックを導入

--- makeChangeLog.bat
+++ makeChangeLog.bat
@@ -8,6 +8,26 @@ set ACCOUNTNAME=sakura-editor
 set PROJECTNAME=sakura
 set OUTFILENAME=CHANGELOG.md
 
+@echo.
+@echo INFO: APPVEYOR_REPO_NAME                   = %APPVEYOR_REPO_NAME%
+@echo INFO: APPVEYOR_PULL_REQUEST_HEAD_REPO_NAME = %APPVEYOR_PULL_REQUEST_HEAD_REPO_NAME%
+@echo.
+
+set SECURE_VAR_IS_VALID=
+if not defined APPVEYOR_PULL_REQUEST_HEAD_REPO_NAME (
+	@rem if APPVEYOR_PULL_REQUEST_HEAD_REPO_NAME is empty, it is not a PR.
+	set SECURE_VAR_IS_VALID=1
+) else if "%APPVEYOR_REPO_NAME%" == "%APPVEYOR_PULL_REQUEST_HEAD_REPO_NAME%" (
+	@rem if APPVEYOR_REPO_NAME and APPVEYOR_PULL_REQUEST_HEAD_REPO_NAME are equal, it is a PR from the same repository.
+	set SECURE_VAR_IS_VALID=1
+)
+
+if not defined SECURE_VAR_IS_VALID (
+	@echo secure variable is valid only in a PR from the same repository or in a branch build.
+	@echo secure variable is available in this build, so skip build.
+	exit /b 0
+)
+
 if not defined CHANGELOG_GITHUB_TOKEN (
 	@echo 1. you need to define env variable CHANGELOG_GITHUB_TOKEN in advance.
 	@echo 2. you can generate GitHub Access Token at https://github.com/settings/tokens/new
